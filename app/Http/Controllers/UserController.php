b020b03a81c94a8ddeaf4edaea1c740e36904599
BernardBerenes <bernardbereness78@gmail.com>
2025-01-06 11:07:09 +0700

    feat: update visibility

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,11 +10,26 @@ class UserController extends Controller
 {
     public function changeVisibility()
     {
-        User::findOrFail(Auth::user()->id)->update([
-            'visibility' => !Auth::user()->visibility
-        ]);
+        $isValid = false;
 
-        return back();
+        if (Auth::user()->visibility && Auth::user()->coin >= 50){
+            $isValid = true;
+            $price = 50;
+        } else if (!Auth::user()->visibility && Auth::user()->coin >= 5){
+            $isValid = true;
+            $price = 5;
+        }
+
+        if ($isValid){
+            User::findOrFail(Auth::user()->id)->update([
+                'coin' => Auth::user()->coin - $price,
+                'visibility' => !Auth::user()->visibility
+            ]);
+
+            return back();
+        } else{
+            return back()->with('error', __('lang.insufficient_coins'));
+        }
     }
 
     public function changeAvatar(Request $request)
